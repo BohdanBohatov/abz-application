bb676030ea38b19b9a63d3a1b4737345eff89c5a
Bohdan <bbohatovmif@gmail.com>
2025-03-04 15:29:13 +0200

    Test php env variables

--- scripts/configure_wp.sh
+++ scripts/configure_wp.sh
@@ -13,29 +13,28 @@ set -e
 
 
 #Export env variables to script
-echo "Start script" > /var/log/debug.log
 source /home/ec2-user/.env
 
 #Retrive credentials from secrets manager to MySql DB
-echo "Retrive secrets" >> /var/log/debug.log
 SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id "$MYSQL_SECRET_ARN" --query 'SecretString' --output text)
 
 MYSQL_USER=$(echo $SECRET_VALUE | jq -r '.username')
 MYSQL_PASSWORD=$(echo $SECRET_VALUE | jq -r '.password')
 
 #Change variables in apache configuration file
-sudo sed -i "s/\$WORDPRESS_DB/${WORDPRESS_DB}/"  /etc/httpd/conf/httpd.conf
-sudo sed -i "s/\$MYSQL_HOST/${MYSQL_HOST}/"  /etc/httpd/conf/httpd.conf
-sudo sed -i "s/\$MYSQL_PORT/${MYSQL_PORT}/"  /etc/httpd/conf/httpd.conf
-sudo sed -i "s/\$MYSQL_USER/${MYSQL_USER}/"  /etc/httpd/conf/httpd.conf
-sudo sed -i "s/\$MYSQL_PASSWORD/${MYSQL_PASSWORD}/"  /etc/httpd/conf/httpd.conf
+sed -i "s/\$WORDPRESS_DB/${WORDPRESS_DB}/"  /etc/httpd/conf/httpd.conf
+sed -i "s/\$MYSQL_HOST/${MYSQL_HOST}/"  /etc/httpd/conf/httpd.conf
+sed -i "s/\$MYSQL_PORT/${MYSQL_PORT}/"  /etc/httpd/conf/httpd.conf
+sed -i "s/\$MYSQL_USER/${MYSQL_USER}/"  /etc/httpd/conf/httpd.conf
+sed -i "s/\$MYSQL_PASSWORD/${MYSQL_PASSWORD}/"  /etc/httpd/conf/httpd.conf
 
-#sudo systemctl restart httpd
+chown apache:apache /etc/httpd/conf/httpd.conf
+systemctl restart httpd
 
-echo "Create database for wordpress" >> /var/log/debug.log
+
 #Create DB for WordPress
 mysql -h $MYSQL_HOST --password=$MYSQL_PASSWORD -u $MYSQL_USER --port=$MYSQL_PORT -D mysql -e "CREATE DATABASE IF NOT EXISTS \`${WORDPRESS_DB}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 
-echo "Script ending" >> /var/log/debug.log
 #Installing and activating cache plugin
-sudo -u ec2-user -i -- wp plugin install w3-total-cache --activate --path=/var/www/html
+#wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --path=/var/www/html
+#sudo -u ec2-user -i -- wp plugin install w3-total-cache --activate --path=/var/www/html
