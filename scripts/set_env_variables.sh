6b8825e022faf0695126f897e027c968dc95f381
Bohdan <bbohatovmif@gmail.com>
2025-03-05 13:29:47 +0200

    Added w3 cache config file, installing wordpress and activating w3 cache plugin

--- scripts/set_env_variables.sh
+++ scripts/set_env_variables.sh
@@ -17,9 +17,16 @@ sed -i "s/\$MYSQL_PORT/${MYSQL_PORT}/"  /etc/httpd/conf/httpd.conf
 sed -i "s/\$MYSQL_USER/${MYSQL_USER}/"  /etc/httpd/conf/httpd.conf
 sed -i "s/\$MYSQL_PASSWORD/${MYSQL_PASSWORD}/"  /etc/httpd/conf/httpd.conf
 
+#Set Redis URL in config file for W3 Total Cache
+sed -i "s/\$REDIS_URL/${REDIS_URL}/"  /home/ec2-user/w3-cache-settings.json
+
 chown apache:apache /etc/httpd/conf/httpd.conf
 chown apache:apache /var/www/html/wp-config.php
 systemctl restart httpd
 
 mysql -h $MYSQL_HOST --password=$MYSQL_PASSWORD -u $MYSQL_USER --port=$MYSQL_PORT -D mysql -e "CREATE DATABASE IF NOT EXISTS \`${WORDPRESS_DB}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 
+#Installing and configuring wordpress
+#TODO store password in secrets manager
+wp core install --url=example.com --title=Title --admin_user=admin --admin_password=passwd --admin_email=example@example.com --path=/var/www/html
+sudo -u ec2-user -i -- wp plugin install w3-total-cache --activate --path=/var/www/html
