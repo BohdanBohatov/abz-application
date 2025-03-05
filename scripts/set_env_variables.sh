0ec7af72f5748385a9154255871b1c603fc169f4
Bohdan <bbohatovmif@gmail.com>
2025-03-05 10:06:44 +0200

    Fixed env variables

--- scripts/set_env_variables.sh
+++ scripts/set_env_variables.sh
@@ -11,12 +11,14 @@ MYSQL_USER=$(echo $SECRET_VALUE | jq -r '.username')
 MYSQL_PASSWORD=$(echo $SECRET_VALUE | jq -r '.password')
 
 #Change variables in apache configuration file
-echo "WORDPRESS_DB=$WORDPRESS_DB" >> /etc/environment
-echo "MYSQL_HOST=$MYSQL_HOST" >> /etc/environment
-echo "MYSQL_PORT=$MYSQL_PORT" >> /etc/environment
-echo "MYSQL_USER=$MYSQL_USER" >> /etc/environment
-echo "MYSQL_PASSWORD=$MYSQL_PASSWORD" >> /etc/environment
+sed -i "s/\$WORDPRESS_DB/${WORDPRESS_DB}/"  /etc/httpd/conf/httpd.conf
+sed -i "s/\$MYSQL_HOST/${MYSQL_HOST}/"  /etc/httpd/conf/httpd.conf
+sed -i "s/\$MYSQL_PORT/${MYSQL_PORT}/"  /etc/httpd/conf/httpd.conf
+sed -i "s/\$MYSQL_USER/${MYSQL_USER}/"  /etc/httpd/conf/httpd.conf
+sed -i "s/\$MYSQL_PASSWORD/${MYSQL_PASSWORD}/"  /etc/httpd/conf/httpd.conf
 
+chown apache:apache /etc/httpd/conf/httpd.conf
+chown apache:apache /var/www/html/wp-config.php
 systemctl restart httpd
 
 mysql -h $MYSQL_HOST --password=$MYSQL_PASSWORD -u $MYSQL_USER --port=$MYSQL_PORT -D mysql -e "CREATE DATABASE IF NOT EXISTS \`${WORDPRESS_DB}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
